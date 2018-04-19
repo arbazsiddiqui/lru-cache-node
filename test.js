6aae673b3d694b1d2346d2a642ea10858a694e56
arbazsiddiqui <arbaz00@gmail.com>
2018-04-19 19:11:50 +0530

    add test for expiry

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const test = require('ava');
 const Cache = require('./src/index');
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 test('initializes a cache correctly', t => {
 	const cache = new Cache(3);
@@ -8,31 +9,95 @@ test('initializes a cache correctly', t => {
 	t.is(typeof cache, 'object')
 });
 
-test('sets new keys in cache', t => {
+test('sets new keys in cache without expiry', t => {
 	const cache = new Cache(3);
+
 	cache.set("Sapiens", 5);
 	t.is(Object.keys(cache.hashMap).length, 1);
+
 	cache.set("Book Thief", 4);
 	t.is(Object.keys(cache.hashMap).length, 2);
+
 	cache.set("Catcher In The Rye", 0);
 	t.is(Object.keys(cache.hashMap).length, 3);
+
 	cache.set("Thus Spoke Zarathustra", 4);
 	t.is(Object.keys(cache.hashMap).length, 3);
 	t.is(cache.hashMap.Sapiens, undefined);
 });
 
-test('get key', t => {
+test('get key without expiry', async t => {
 	const cache = new Cache(3);
+
 	cache.set("Sapiens", 5);
 	t.is(Object.keys(cache.hashMap).length, 1);
+
 	cache.set("Book Thief", 4);
 	t.is(Object.keys(cache.hashMap).length, 2);
+
 	cache.set("Catcher In The Rye", 0);
 	t.is(Object.keys(cache.hashMap).length, 3);
+
+	await sleep(10);
+
 	const sapiensRating = cache.get("Sapiens");
 	t.is(sapiensRating, 5);
+
 	cache.set("Thus Spoke Zarathustra", 4);
 	t.is(Object.keys(cache.hashMap).length, 3);
+
 	t.is(cache.hashMap.Sapiens.content.value, 5);
 	t.is(cache.hashMap["Book Thief"], undefined);
+});
+
+test('sets with expiry', t => {
+	const cache = new Cache(3, 10);
+
+	cache.set("Sapiens", 5);
+	t.is(cache.maxAge, 10);
+	t.is(cache.hashMap.Sapiens.maxAge, 10);
+
+	cache.set("Book Thief", 4, 7);
+	t.is(cache.hashMap["Book Thief"].maxAge, 7);
+
+	cache.set("Catcher In The Rye", 0);
+	t.is(cache.hashMap["Catcher In The Rye"].maxAge, 10);
+});
+
+test('gets with expiry', async t => {
+	const cache = new Cache(3, 10);
+	cache.set("Sapiens", 5);
+	const sapiens = cache.get("Sapiens");
+	t.is(sapiens, 5);
+
+	await sleep(3);
+	const sapiensAfter3s = cache.get("Sapiens");
+	t.is(sapiensAfter3s, 5);
+
+	await sleep(11);
+	const sapiensAfter11s = cache.get("Sapiens");
+	t.is(sapiensAfter11s, null);
+	t.is(cache.hashMap.Sapiens, undefined);
+
+	cache.set("Book Thief", 4,  25);
+	const bookThief = cache.get("Book Thief");
+	t.is(bookThief, 4);
+
+	await sleep(10);
+	const bookThiefAfter10s = cache.get("Book Thief");
+	t.is(bookThiefAfter10s, 4);
+
+	await sleep(10);
+	const bookThiefAfter20s = cache.get("Book Thief");
+	t.is(bookThiefAfter20s, 4);
+
+	await sleep(10);
+	const bookThiefAfter30s = cache.get("Book Thief");
+	t.is(bookThiefAfter30s, 4);
+
+	await sleep(30);
+	const bookThiefAfter30sWithoutReset = cache.get("Book Thief");
+	t.is(bookThiefAfter30sWithoutReset, null);
+
+
 });
